e9d1b3b4628772c678b7234148908df6ca70389f
Paolo -Willy- Nicorelli <p.nicorelli@gmail.com>
2017-01-02 15:30:18 +0100

    push websites lists online for testing

--- listHandler.js
+++ listHandler.js
@@ -1,21 +1,38 @@
 'use strict'
 
 var listHandler = {
-  'init': ()=>{
+  'readIndex': (  )=>{
     // get the index from github
-    var raw = 'https://raw.githubusercontent.com/berriex/scepticism/lists/index.json';
+    var raw = 'https://raw.githubusercontent.com/berriex/scepticism/master/lists/index.json';
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.open('GET', raw, true);
     xmlhttp.onreadystatechange = function() {
         if (xmlhttp.readyState == 4) {
             if(xmlhttp.status == 200) {
-                var rawList = JSON.parse(xmlhttp.responseText);
+                listHandler.indexList = JSON.parse(xmlhttp.responseText);
+                for( var i=0; i< listHandler.indexList.length; i++){
+                  listHandler.downloadList( i );
+                }
 
-                console.log( rawList );
              }
         }
     };
     xmlhttp.send(null);
+  },
+
+  'downloadList': ( index )=>{
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.open('GET', listHandler.indexList[index].url, true);
+        xmlhttp.onreadystatechange = function() {
+            if (xmlhttp.readyState == 4) {
+                if(xmlhttp.status == 200) {
+                    var result = JSON.parse(xmlhttp.responseText);
+                    listHandler.indexList[index]['urls'] = result.urls;
+                    listHandler.indexList[index]['message'] = result.message;
+                 }
+            }
+        };
+        xmlhttp.send(null);
   },
 
   'getAll': ()=>{
@@ -28,4 +45,4 @@ var listHandler = {
 }
 
 
-listHandler.init();
+listHandler.readIndex();
